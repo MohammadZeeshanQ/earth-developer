5c9ebd1724009f206b4ea6e4f8a09da5e82360ff
MohammadZeeshanQ <zaqil1976@gmail.com>
2021-05-10 09:45:55 +0300

    Add: Content for Responsive Views

---
+++ src/component/pages/ProjectView.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {mnakeStyles} from '@material-ui/core/styles'
+import {Button, Typography} from '@material-ui/core'
+
+const useStyles = makeStyles({
+    root:{
+
+    },
+    wrapper:{
+
+    },
+});
+
+export default function ProjectView() {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <div className={classes.wrapper}>
+                
+            </div>
+        </div>
+    )
+}
