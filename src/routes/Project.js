45e6660db8409d956a5ba87293ece8d286f87d83
Zeshan <mohammadzeshan610@yahoo.com>
2021-07-06 07:20:19 +0300

    Add: Dialog Box for Project

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Grid, Chip } from '@material-ui/core';
-
+import { Typography, Grid, Chip, Dialog, DialogTitle, DialogContent, DialogActions, Button, IconButton, } from '@material-ui/core';
 import { gridData } from '../middleware/ProjectData.js';
+import CloseIcon from '@material-ui/icons/Close';
 
 
 const useStyles = makeStyles({
@@ -152,16 +152,73 @@ const useStyles = makeStyles({
             margin: '.3rem .2rem 0 0'
         },
     },
+
+    dialogImage: {
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+        borderRadius: '1rem 1rem 0 0',
+
+        '@media(max-width: 600px)': {
+            objectFit: 'contain'
+        },
+
+        '@media(min-width: 601px) and (max-width: 1024px)': {
+
+        },
+    },
+
+    dialogButton: {
+        border: '1px solid #636363',
+        color: '#636363',
+        fontFamily: 'Oxy-Reg',
+        letterSpacing: '0.6px',
+        margin: '1rem 0',
+
+        '&:hover': {
+            backgroundColor: '#FF4F5A',
+            color: '#fcfcfc',
+            transition: '.4s ease all',
+        },
+
+        '@media(max-width: 600px)': {
+            margin: '.5rem 0',
+        },
+    },
+
+    dialogTitle: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+    },
 });
 
 
 export default function Project() {
 
     const classes = useStyles();
+    const [dialog, setDialog] = useState(false);
+    const [pictureIndex, setPictureIndex] = useState([])
+    const [dialogLink, setDialongLink] = useState('');
+
+
+    const toggleDialog = () => {
+        setDialog(!dialog);
+    };
+
+
+    const dialogData = (image, link) => {
+        toggleDialog();
+        setPictureIndex(image);
+        setDialongLink(link);
+    };
+
 
     useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [])
+        window.scrollTo(0, 0);
+
+        console.log(pictureIndex);
+    }, [pictureIndex, dialogLink])
+
 
     return (
         <div className={classes.root}>
@@ -191,35 +248,61 @@ export default function Project() {
                                 sm={4}
                                 md={3}
                                 className={classes.gridItem}
+                                onClick={() => dialogData(image, link)}
                             >
-                                <a href={link} style={{ textDecoration: 'none', color: '#1c1c1c', }}>
-                                    <div className={classes.gridImageContainer}>
-                                        <img className={classes.gridImage} src={image} alt='Project Images' />
-                                    </div>
+                                <div className={classes.gridImageContainer}>
+                                    <img className={classes.gridImage} src={image} alt='Project Images' />
+                                </div>
 
-                                    <div className={classes.gridTextContainer}>
-                                        <Typography className={classes.title} variant='h6'>
-                                            {title}
-                                        </Typography>
+                                <div className={classes.gridTextContainer}>
+                                    <Typography className={classes.title} variant='h6'>
+                                        {title}
+                                    </Typography>
 
-                                        <Typography className={classes.description} variant='body2'>
-                                            {description}
-                                        </Typography>
+                                    <Typography className={classes.description} variant='body2'>
+                                        {description}
+                                    </Typography>
 
-                                        {
-                                            program.map((programs, i) =>
-                                                <Chip className={classes.chipTitle} label={programs.code} />
-                                            )
-                                        }
+                                    {
+                                        program.map((programs, i) =>
+                                            <Chip className={classes.chipTitle} label={programs.code} />
+                                        )
+                                    }
 
-                                    </div>
-                                </a>
+                                </div>
                             </Grid>
 
                         )
                     }
 
                 </Grid>
+
+                <Dialog
+                    maxWidth='md'
+                    open={dialog}
+                    onClose={toggleDialog}
+                >
+                    <DialogTitle className={classes.dialogTitle}>
+                        <IconButton onClick={toggleDialog} size='small'>
+                            <CloseIcon style={{ color: '#636363' }} />
+                        </IconButton>
+                    </DialogTitle>
+
+                    <DialogContent>
+                        <img src={pictureIndex} className={classes.dialogImage} alt='Projects' />
+                    </DialogContent>
+
+                    <DialogActions>
+                        <Button
+                            className={classes.dialogButton}
+                            href={dialogLink}
+                            variant='outlined'
+                        >
+                            Source Code
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+
             </div>
         </div>
     )
